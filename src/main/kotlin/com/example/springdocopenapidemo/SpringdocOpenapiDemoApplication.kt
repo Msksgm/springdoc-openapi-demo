57f3a48f4a3055dd6716f6fad3463d8f7282f808
Msksgm <msksgm3124@gmail.com>
2023-12-24 19:30:24 +0900

    chore: リポジトリを初期化

---
+++ src/main/kotlin/com/example/springdocopenapidemo/SpringdocOpenapiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdocopenapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocOpenapiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringdocOpenapiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdocopenapidemo/SpringdocOpenapiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdocopenapidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocOpenapiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
